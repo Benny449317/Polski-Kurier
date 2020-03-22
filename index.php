1573556d6733d03cd560e4444d36af57a9db94c2
Beniamin <59067835+BeniaminKolinka@users.noreply.github.com>
2020-03-22 22:28:13 +0100

    Moja pierwsza strona, dodane lokalne pliki, pierwszy commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>www.polskikurier.pl</title>
+    <link rel="icon" href="images/icon.png" type="image/png" size="16x16">
+    <link rel = "stylesheet"  type ="text/css" href = "css files/styles.css" />
+
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+  </head>
+  <body>
+<div id="headcontainer">
+
+    <div id="headbar">
+        <ul id="head_ul">
+          <li class="head_ul_li"><a href="">Klienci biznesowi</a></li>
+          <li class="head_ul_li"><a href="">Wesprzyj</a></li>
+          <li class="head_ul_li"><a href="">Forum PolskiKurier</a></li>
+          <li class="head_ul_li"><a href="">Dołącz do nas</a></li>
+          <li class="head_ul_li"><a href="">Polityka prywatności</a></li>
+        </ul>
+    </div>
+
+    <div id="header">
+      <a href="index.php"><img src="images/logo.png" alt="logo.png"></a>
+    </div>
+
+    <div id="menu">
+      <ul id="menu_ul">
+        <a href="check.php"><li class="menu_ul_li">Zarządzaj przesyłką</li></a>
+        <a href="order.php"><li class="menu_ul_li">Nadaj paczkę</li></a>
+        <a href="delivery_point.php"><li class="menu_ul_li">Punkt Polski Kurier</li></a>
+        <a href="prizes.php"><li class="menu_ul_li">Cennik</li></a>
+        <a href="refound.php"><li class="menu_ul_li">Zwroty</li></a>
+      </ul>
+
+    </div>
+
+</div>
+
+
+
+
+    <div id="imagecontainer">
+
+        <div class="imagebox">
+            <img src="images/image1.jpg" alt="image1">
+        </div>
+        <div class="imagebox">
+            <img src="images/image2.jpg" alt="image2">
+        </div>
+        <div class="imagebox">
+            <img src="images/image3.jpg" alt="image3">
+
+<!--
+          <div class="image_text">
+
+              <a href="">
+
+                  <div class="image_text_div">
+                    <div class= "image_text_div_p">
+                      <p class="image_text2_p">Polski Kurier</p>
+                      <p class="image_text2_p2">Wyślij paczkę teraz</p>
+                    </div>
+                </div>
+                <div class="">
+                  <div class="image_text2_div">
+                  <i class="fas fa-angle-right" style='font-size:120px;color:red;'></i>
+                </div>
+              </div>
+              </a>
+          </div>
+-->
+        </div>
+
+    </div>
+
+    <div id="maincontainer">
+        <div id="image_main1">
+            <img src="images/image4.jpg" alt="image4">
+        </div>
+
+        <div class="info">
+          <span class="span_text">
+            <p class="p_text">Sprawdź aktualne miejsce pobytu paczki dzięki naszej aplikacji "Polski Kurier Mobile"!<p></br>
+            <p class="p_text">Śledź swoją paczkę 24/7 przy pomocy naszej aplikacji, jest zupełnie darmowa. Spróbuj już teraz!<p>
+          </span>
+        </div>
+
+        <div class="data">
+
+            <p class="p_headline">Nie wiesz gdzie znajduje się twoja paczka?</p>
+            <a href="check.php">
+              <div id="button1">
+                <span>
+                  <p class=""="p_text2">Zarządzaj swoją paczką w każdej chwili!</p>
+                </span>
+              </div>
+            </a>
+              <span>
+                <p class="text_area1">Aplikacja Polski Kurier jest możliwa do pobrania na urządzenia z systemem android oraz IOS, nie czekaj - sprawdź dokładnie kiedy przyjdzie
+                Twoja paczka.</p>
+              </span>
+              <p class="text_area2">Po zalogowaniu w serwisie polskikurier.paczka.pl otrzymasz możliwość:</p>
+            </span>
+            <div>
+              <ul class="ul_textarea">
+                <li>-śledzenia poruszania się twojej paczki</li>
+                <li>-dostępu do dynamicznego okreslania miejsca odbioru paczki</li>
+                <li>-50 złotych na pierwsze trzy kolejne paczki wysłane z aplikacji mobilnej!</li>
+                <li>-odmowy przyjęcia paczki</li>
+                <li>-zmiany terminu odbioru Twojej paczki</li>
+
+              </ul>
+           </div>
+           <span>
+             <p class="text_area3">U nas możesz zamówić paczki każdych rozmiarów, do 50kg wagi paczki.</p>
+             <p class="text_area3">Po prostu zamów, a nasz kurier przyjedzie zabrać twoją przesyłkę w przeciągu jednego dnia roboczego!</p>
+             <p class="text_area3">Posiadamy 347 punktów PolskiKurier w całej Polsce a ich ilość z roku na rok wzrasta!</p>
+             <p class="text_area3">Podejmujemy się również odbiorów stałych, cyklicznych z okreleniem miejsca oraz godziny</p>
+             <p class="text_area3">Współpracujemy z 78 firmami z zakresu logistyki oraz transportu, nasze Punkty PolskiKurier lub nasi
+                podwykonawcy znajdują się w każdym mieście powyżej 30 tysięcy mieszkańców. Jeśli posiadasz własną firmę zajmującą się logistyką bądź transportem, skontaktuj się z nami!</p>
+           </span>
+
+
+        </div>
+
+
+        <div id="image_main2">
+          <img src="images/image5.jpg" alt="image5">
+      </div>
+
+      <div id="footer">
+        <p class="p_foot">Strona wykonana w ceu pokazania moich umiejętności w zakresie tworzenia stron internetowych.</p>
+        <p class="p_foot">Strona nie jest wykonana w celach zarobkowych.</p>
+        <p class="p_foot">Zdjęcia użyte ze strony Unsplash.com</p>
+      </div>
+    </div>
+  </body>
+</html>
